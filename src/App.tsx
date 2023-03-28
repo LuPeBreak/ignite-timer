f53b011f334bfc793477a52e457b97115fca13e2
LuPeBreak <lfbmrj15@gmail.com>
2023-03-28 19:47:27 -0300

    inicio do projeto - projeto iniciado com vite - retirado arquivos e importaçoes desnecessarias para o projeto

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+
+export function App() {
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+    </div>
+  );
+}
