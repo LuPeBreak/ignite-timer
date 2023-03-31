40399550d95dce4dad4ce5a568ff6769d5394a48
LuPeBreak <lfbmrj15@gmail.com>
2023-03-31 07:46:57 -0300

    Pre finalizado estilo do history

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,7 +1,56 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
 export function History() {
   return (
-    <div>
-      <h1>History</h1>
-    </div>
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa1</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa2</td>
+              <td>20 minutos</td>
+              <td>Há 4 meses</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa3</td>
+              <td>20 minutos</td>
+              <td>Há 12 meses</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa4</td>
+              <td>20 minutos</td>
+              <td>Há 3 meses</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
   )
 }
